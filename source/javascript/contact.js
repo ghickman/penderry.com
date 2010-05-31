7bc1df4bb9bc2b54db58944d91c7f894ab84fe06
George Hickman <george@ghickman.co.uk>
2010-05-31 08:08:32 +0100

    updated blur call to add error div

--- source/javascript/contact.js
+++ source/javascript/contact.js
@@ -1,16 +1,15 @@
 $(function() {
-    // on focus lost -> none are empty
     /**
      * Attaches a lostFocus event to the given selector and adds a warning div below if the user
      * does not fill in the 
      */
     function not_empty_on_focus_lost(selector) {
-        $(selector + '').lostFocus(function() {
-            $(selector + '').append('div', 'error')
+        $('#'+selector).blur(function() {
+            $('div.'+selector).append('<div class="error">NO NAME</div>')
         });
     }
     
-    not_empty_on_focus_lost('input.name')
+    not_empty_on_focus_lost('name')
     not_empty_on_focus_lost('input.email')
     not_empty_on_focus_lost('input.message')
     
@@ -18,7 +17,7 @@ $(function() {
     /**
      * Check to see that the email address is valid when focus is lost.
      */
-    if($('input.email').lostFocus()) {
-        $('input.email').validate()
-    }
+    // if($('input.email').lostFocus()) {
+    //         $('input.email').validate()
+    //     }
 });
