b79c5629847a4c1bb3021a19f7080a5b8ac5013f
George Hickman <george@ghickman.co.uk>
2013-02-07 14:27:40 +0000

    Migrate to pelican

---
+++ pelicanconf.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'George Hickman'
+
+DEFAULT_LANG = u'en'
+
+DEFAULT_PAGINATION = False
+
+DISPLAY_PAGES_ON_MENU = True
+
+SITENAME = u'Penderry.com'
+
+TEMPLATE_PAGES = {
+    'holiday_cottages.html': 'holiday_cottages.html',
+    'things_to_do.html': 'things_to_do.html',
+    'farm_and_habitats.html': 'farm_and_habitats.html',
+    'art_projects.html': 'art_projects.html',
+    'tina_lewis_paintings.html': 'tina_lewis_paintings.html',
+    'contact.html': 'contact.html',
+}
+
+THEME = 'theme'
+
+TIMEZONE = 'Europe/London'
+
+PAINTINGS = [{
+    'url': 'http://farm5.static.flickr.com/4066/4652164363_6757e2b8e1',
+    'alt': '7 Tina Lewis, April 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4028/4652780538_e662ce79e8',
+    'alt': '2 Aug 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4019/4652781090_d5aceece4d',
+    'alt': 'Tina Lewis Sheep April 07',
+}, {
+    'url': 'http://farm5.static.flickr.com/4027/4652781380_3a2b6989bf',
+    'alt': 'Tina Lewis Field of sheep April 07',
+}, {
+    'url': 'http://farm5.static.flickr.com/4015/4652764078_2d24f22a7c',
+    'alt': 'T_Lewis',
+}, {
+    'url': 'http://farm5.static.flickr.com/4065/4652143627_4ee3f027d1',
+    'alt': 'T_Lewis-9',
+}, {
+    'url': 'http://farm5.static.flickr.com/4065/4652143829_b73b899d2d',
+    'alt': 'T_Lewis-12',
+}, {
+    'url': 'http://farm5.static.flickr.com/4049/4655778471_3dba163ebf',
+    'alt': 'Hey pig at home',
+}, {
+    'url': 'http://farm5.static.flickr.com/4025/4652143707_7668e18da1',
+    'alt': 'T_Lewis-10',
+}, {
+    'url': 'http://farm5.static.flickr.com/4060/4652144061_a9ac2ccf15',
+    'alt': 'T_Lewis-5',
+}, {
+    'url': 'http://farm5.static.flickr.com/4038/4652763822_e288c89e0e',
+    'alt': 'T_Lewis-4',
+}, {
+    'url': 'http://farm5.static.flickr.com/4058/4652781676_fef0692ee1',
+    'alt': 'Tina Lewis Farmhouse April 07',
+}, {
+    'url': 'http://farm5.static.flickr.com/4065/4655780041_35a3d97c59',
+    'alt': 'Opium Poppies',
+}, {
+    'url': 'http://farm5.static.flickr.com/4006/4652162525_93149f4b60',
+    'alt': 'Cherry Blossom web',
+}, {
+    'url': 'http://farm5.static.flickr.com/4019/4652779824_44aca74c14',
+    'alt': '7 Tina Lewis April 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4016/4652763966_d5aa0d28ab',
+    'alt': 'Untitled-6',
+}, {
+    'url': 'http://farm5.static.flickr.com/4043/4652763756_e9e06d57ff',
+    'alt': 'T_Lewis-13',
+}, {
+    'url': 'http://farm5.static.flickr.com/4033/4652780044_48d2baf68f',
+    'alt': '1 Tina Lewis April 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4024/4652156303_a23559c6d7',
+    'alt': '5 Tina Lewis April 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4015/4652771734_6cea2d3018',
+    'alt': '3 Tina Lewis April 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4026/4652162799_895d08189f',
+    'alt': '2 Tina Lewis Oct 07',
+}, {
+    'url': 'http://farm5.static.flickr.com/4062/4652782878_b55c396f8e',
+    'alt': '2 Tina Lewis, April 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4063/4652782660_7fd97b61b1',
+    'alt': '1 Tina Lewis, April 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4015/4652782480_6ede1fe9b1',
+    'alt': '1 Tina Lewis Oct 07',
+}, {
+    'url': 'http://farm5.static.flickr.com/4012/4652164639_127b879be4',
+    'alt': '1Tina Lewis May 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4017/4652166183_a49917bcb3',
+    'alt': 'Paddling, June 08 C',
+}, {
+    'url': 'http://farm5.static.flickr.com/4052/4652784492_ed18cfb408',
+    'alt': '2Tina Lewis May 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4055/4652773888_83790aa0e7',
+    'alt': '4 Tina Lewis April 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4051/4652162283_aaaf5dd021',
+    'alt': 'Tina Lewis Beach and boy April 07',
+}, {
+    'url': 'http://farm5.static.flickr.com/4051/4652786054_e0ef28cbe6',
+    'alt': 'Paddling, June 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4013/4655776081_5f8dbc158e',
+    'alt': 'Storming Seagull',
+}, {
+    'url': 'http://farm5.static.flickr.com/4017/4652161107_e0877df36c',
+    'alt': '3 Aug 06',
+}, {
+    'url': 'http://farm5.static.flickr.com/4036/4652785000_6f54da63c2',
+    'alt': '5Tina Lewis May 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4045/4652784674_1900d7b8ff',
+    'alt': '3Tina Lewis May 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4042/4652784854_506f1b705e',
+    'alt': '4Tina Lewis May 08',
+}, {
+    'url': 'http://farm5.static.flickr.com/4010/4652143749_c31f8b536c',
+    'alt': 'T_Lewis-11',
+}]
+
