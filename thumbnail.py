d907aff7c06c1efdfabf39a1dde912d9047436fa
George Hickman <george@ghickman.co.uk>
2013-09-01 12:26:01 +0100

    Get rid of an errant print

--- thumbnail.py
+++ thumbnail.py
@@ -28,5 +28,4 @@ for fn in [f for f in sys.argv[1:] if not 'thumb' in f]:
     img.thumbnail(THUMB_SIZE, Image.ANTIALIAS)
 
     base, ext = os.path.splitext(fn)
-    print(ext)
     img.save('{}{}{}'.format(base, '_thumb', ext))
