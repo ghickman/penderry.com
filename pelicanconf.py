5150352bfb21ba270028f55feb9b6fc8491ea436
George Hickman <george@ghickman.co.uk>
2013-09-01 11:34:35 +0100

    Major Update
    * Move to s3 for hosting
    * Update all of Tina's images
    * Remove second cottage
    * Update things to do links
    * Move images into repo

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,10 +9,12 @@ DEFAULT_PAGINATION = False
 
 DISPLAY_PAGES_ON_MENU = True
 
+IMAGE_URL = '/theme/images/'
+
 SITENAME = u'Penderry Farm'
 
 TEMPLATE_PAGES = {
-    'holiday_cottages.html': 'holiday_cottages.html',
+    'holiday_cottage.html': 'holiday_cottage.html',
     'things_to_do.html': 'things_to_do.html',
     'farm_and_habitats.html': 'farm_and_habitats.html',
     'art_projects.html': 'art_projects.html',
@@ -24,41 +26,36 @@ THEME = 'theme'
 
 TIMEZONE = 'Europe/London'
 
-PAINTINGS = [{
-    'url': 'http://farm5.static.flickr.com/4066/4652164363_6757e2b8e1',
-    'alt': '7 Tina Lewis, April 08',
-}, {
-    'url': 'http://farm5.static.flickr.com/4028/4652780538_e662ce79e8',
-    'alt': '2 Aug 06',
-}, {
-    'url': 'http://farm5.static.flickr.com/4027/4652781380_3a2b6989bf',
-    'alt': 'Tina Lewis Field of sheep April 07',
-}, {
-    'url': 'http://farm5.static.flickr.com/4065/4655780041_35a3d97c59',
-    'alt': 'Opium Poppies',
-}, {
-    'url': 'http://farm5.static.flickr.com/4006/4652162525_93149f4b60',
-    'alt': 'Cherry Blossom web',
-}, {
-    'url': 'http://farm5.static.flickr.com/4019/4652779824_44aca74c14',
-    'alt': '7 Tina Lewis April 06',
-}, {
-    'url': 'http://farm5.static.flickr.com/4026/4652162799_895d08189f',
-    'alt': '2 Tina Lewis Oct 07',
-}, {
-    'url': 'http://farm5.static.flickr.com/4062/4652782878_b55c396f8e',
-    'alt': '2 Tina Lewis, April 08',
-}, {
-    'url': 'http://farm5.static.flickr.com/4063/4652782660_7fd97b61b1',
-    'alt': '1 Tina Lewis, April 08',
-}, {
-    'url': 'http://farm5.static.flickr.com/4015/4652782480_6ede1fe9b1',
-    'alt': '1 Tina Lewis Oct 07',
-}, {
-    'url': 'http://farm5.static.flickr.com/4017/4652166183_a49917bcb3',
-    'alt': 'Paddling, June 08 C',
-}, {
-    'url': 'http://farm5.static.flickr.com/4051/4652162283_aaaf5dd021',
-    'alt': 'Tina Lewis Beach and boy April 07',
-}]
-
+names = [
+    'Tina Lewis April 06',
+    'Tina Lewis April 07 Beach And Boy',
+    'Tina Lewis April 07 Field Of Sheep',
+    'Tina Lewis April 08 1',
+    'Tina Lewis April 08 2',
+    'Tina Lewis April 08 3',
+    'Tina Lewis April 13 1',
+    'Tina Lewis April 13 2',
+    'Tina Lewis April 13 3',
+    'Tina Lewis April 13 4',
+    'Tina Lewis April 13 5',
+    'Tina Lewis April 13 6',
+    'Tina Lewis April 13 7',
+    'Tina Lewis April 13 8',
+    'Tina Lewis April 13 9',
+    'Tina Lewis Aug 06',
+    'Tina Lewis Aug 12 1',
+    'Tina Lewis Cherry Blossom Web',
+    'Tina Lewis July 10',
+    'Tina Lewis June 08 Paddling',
+    'Tina Lewis June 12 1',
+    'Tina Lewis June 12 2',
+    'Tina Lewis June 12 3',
+    'Tina Lewis June 12 4',
+    'Tina Lewis Oct 07 1',
+    'Tina Lewis Oct 07 2',
+    'Tina Lewis Oct 11. 2Jpeg',
+    'Tina Lewis Oct 11 1',
+    'Tina Lewis Oct 11 3',
+    'Tina Lewis Opium Poppies',
+]
+PAINTINGS = [{'alt': x, 'name': x.lower().replace(' ', '_')} for x in names]
